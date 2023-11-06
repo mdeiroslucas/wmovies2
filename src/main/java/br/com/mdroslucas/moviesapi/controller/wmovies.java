6f32573f41fcce0ca199185a6898726899255b39
mdroslucas <lucas-medeiros@live.com>
2023-11-06 07:45:01 -0300

    The elements of the JSON are now being retrieved properly

--- src/main/java/br/com/mdroslucas/moviesapi/controller/wmovies.java
+++ src/main/java/br/com/mdroslucas/moviesapi/controller/wmovies.java
@@ -2,10 +2,10 @@ package br.com.mdroslucas.moviesapi.controller;
 
 
 import br.com.mdroslucas.moviesapi.model.movie.DadosMovieTMDB;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -37,6 +38,7 @@ public class wmovies {
 
     @GetMapping("/now")
     @ResponseStatus(HttpStatus.OK)
+//    public String dadosMovice () throws IOException, InterruptedException {
     public List<DadosMovieTMDB> dadosMovice () throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri + "/discover/movie?include_adult=true&language=pt-br"))
@@ -48,20 +50,50 @@ public class wmovies {
         HttpClient client = HttpClient.newHttpClient();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        JsonNode jsonNode = new ObjectMapper().readTree(response.body()).get("results");
+        //        HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+
+        //        ObjectMapper objectMapper = new ObjectMapper();
+//        List<DadosMovieTMDB> dadosMoviesList = objectMapper.readValue(response.body(), new TypeReference<List<DadosMovieTMDB>>() {
+//        });
+
+        Gson gson = new Gson();
+
+        JsonObject jsonObject = gson.fromJson(response.body(), JsonObject.class);
+
+        JsonArray resultsArray = jsonObject.get("results").getAsJsonArray();
+
         List<DadosMovieTMDB> dadosMoviesList = new ArrayList<>();
-        jsonNode.elements().forEachRemaining(movie -> {
-            dadosMoviesList.add(new DadosMovieTMDB(
-                    movie.get("title").toString(), movie.get("overview").toString(),
-                    movie.get("release_date").toString(), movie.get("backdrop_path").toString(),
-                    movie.get("vote_average").asDouble()));
-        });
 
-        dadosMoviesList.stream().forEach(movie -> System.out.println(movie.toString()));
-        System.out.println(response.body());
+        for (JsonElement movieElement : resultsArray) {
+            JsonObject movieObject = movieElement.getAsJsonObject();
+            DadosMovieTMDB dadosMovie = new DadosMovieTMDB(
+                    movieObject.get("title").getAsString(), movieObject.get("overview").getAsString(),
+                    movieObject.get("release_date").getAsString(), movieObject.get("backdrop_path").getAsString(),
+                    movieObject.get("vote_average").getAsDouble()
+            );
+            dadosMoviesList.add(dadosMovie);
+        }
 
-        System.out.println(jsonNode.toString());
 
+
+
+//        System.out.println(response.body());
+
+//        JsonNode jsonNode = new ObjectMapper().readTree(response.body()).get("results");
+//        List<DadosMovieTMDB> dadosMoviesList = new ArrayList<>();
+//        jsonNode.elements().forEachRemaining(movie -> {
+//            dadosMoviesList.add(new DadosMovieTMDB(
+//                    movie.get("title").toString(), movie.get("overview").toString(),
+//                    movie.get("release_date").toString(), movie.get("backdrop_path").toString(),
+//                    movie.get("vote_average").asDouble()));
+//        });
+//
+//        dadosMoviesList.stream().forEach(movie -> System.out.println(movie.title()));
+//        System.out.println(response.body().toString());
+
+        System.out.println(dadosMoviesList);
+
+//        return response.body();
         return dadosMoviesList;
     }
 }
